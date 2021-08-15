54e29c157bde6445f041a13a7fa60e96b4b19d36
Dhruv Rathore <dhruv.rathore99@gmail.com>
2021-08-15 14:21:53 +0530

    styling done

--- isTriangle.js
+++ isTriangle.js
@@ -1,26 +1,3 @@
-// const inputs = document.querySelectorAll(".angle-input");
-// const isTriangleBtn = document.querySelector("#isTraingle-btn");
-// const output = document.querySelector("#output");
-
-// function calculateSumOfAngles(angle1, angle2, angle3) {
-//     const sumOfAngles = angle1 + angle2 + angle3;
-//     return sumOfAngles;
-//   }
-
-// function isTrinagle() {
-    
-//     const total_angle = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
-
-//     if(total_angle == 180){
-//         output.innerHTML= "Yes, these angles can form a triangle";
-//     }else{
-//         output.innerHTML="The given Angles won't be ablle to make a Triangle";
-//     }
-    
-// }
-
-// isTriangleBtn.addEventListener("click", isTriangle);
-
 const inputs = document.querySelectorAll(".angle-input");
 const output = document.querySelector("#output");
 const isTriangleBtn = document.querySelector("#isTraingle-btn");
